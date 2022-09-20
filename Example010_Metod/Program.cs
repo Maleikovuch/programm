8f0db0494ad0dd8bf2bf0d2899bf5145fd2fdb61
«maleikovuch» <maleikovuch@mail.ru>
2022-09-21 02:41:22 +0400

    Добавили новые задачи из 2-ой лекции (008, 009, 010, 011)

---
+++ Example010_Metod/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arroy = { 11, 25, 31, 45, 22, 15, 74, 31, 22 };
+
+int n = arroy.Length;
+int find = 22;
+
+int index = 0;
+
+while (index < n)
+{
+    if (arroy[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+
+        index++;
+}
---
+++ Examples009_IntroArrow/Program.cs
@@ -0,0 +1,13 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] arroy = { 11, 23, 43, 74, 590, 26, 17, 38, 91 };
+
+
+int result = Max(Max(arroy[0], arroy[1], arroy[2]),Max(arroy[3], arroy[4], arroy[5]),Max(arroy[6], arroy[7], arroy[8]));
+Console.WriteLine(result);
